52e71b6735da265c3068b57cd0e66720814cd2bc
YuraYuchkavich <nhyyhn@tut.by>
2017-02-05 23:05:40 +0300

    yesssss, 3 дня мучался

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+        this.array = [];
+        this.startIndex = null;
+        this.endIndex = null;
+        this.middleIndex = null;
+    }
 
     setRange(min, max) {
+        for (let i = min; i <= max; i++) {
+            this.array.push(i);
+        }
 
+        this.startIndex = 0;
+        this.endIndex = this.array.length - 1;
     }
 
     guess() {
+        this.middleIndex = (this.startIndex + this.endIndex) / 2;
+        this.middleIndex = Math.ceil(this.middleIndex);
 
+        return this.array[this.middleIndex];
     }
 
     lower() {
-
+        this.endIndex = this.middleIndex;
     }
 
     greater() {
-
+        this.startIndex = this.middleIndex;
     }
 }
 
